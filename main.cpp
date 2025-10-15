738344a53cf0e3b1badeddd8d67eb201044ad623
WJ <boss403@naver.com>
2025-10-16 01:51:01 +0900

    마우스 이벤트

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <opencv2/opencv.hpp>
 
 
+void onMouse(int event, int x, int y, int flags, void* userdata)
+{
+    if(event == cv::EVENT_LBUTTONDBLCLK)
+    {
+        std::cout<<"왼쪽 버튼 클릭"<<std::endl;
+    }
+}
+
 int main(void)
 {
     cv::Mat image1(300, 400, CV_8U, cv::Scalar(150));
@@ -11,10 +19,13 @@ int main(void)
     cv::moveWindow(name1, 0, 0);
     cv::resizeWindow(name1, 100,100);
 
+    cv::setMouseCallback(name1, onMouse,0);
+
     while(1)
     {
         cv::imshow(name1, image1);
         int  key = cv::waitKey(1);
+        
 
         if(key == 27)
         {
