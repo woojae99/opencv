8801ce7a946c576720c625c0aa9c2fb0ab541732
WJ <boss403@naver.com>
2025-10-21 00:48:29 +0900

    도형 그리기

--- main.cpp
+++ main.cpp
@@ -1,44 +1,26 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-
-void onMouse(int event, int x, int y, int flags, void* userdata)
-{
-    if(event == cv::EVENT_LBUTTONDBLCLK)
-    {
-        std::cout<<"왼쪽 버튼 클릭"<<std::endl;
-    }
-}
-
 int main(void)
 {
-    cv::Mat image1(300, 400, CV_8U, cv::Scalar(150));
-    std::string name1 = "white";
+    cv::Scalar blue(255, 0, 0), green(0, 255, 0), red(0, 0, 255), white(255, 255, 255);
+    cv::Mat image(800, 800, CV_8UC3, white);
 
-    cv::namedWindow(name1, cv::WINDOW_NORMAL);
-    cv::moveWindow(name1, 0, 0);
-    cv::resizeWindow(name1, 100,100);
+    cv::Point pt1(100, 100), pt2(200, 200), pt3(300, 300);
+    cv::Rect r1(pt1, cv::Size(50, 50));
 
-    cv::setMouseCallback(name1, onMouse,0);
+    cv::line(image, cv::Point(100, 100), cv::Point(200, 100), red, 1, cv::LINE_4);
+    cv::line(image, cv::Point(100, 110), cv::Point(200, 110), red, 1, cv::LINE_4);
 
-    while(1)
-    {
-        cv::imshow(name1, image1);
-        int  key = cv::waitKey(1);
-        
+    cv::rectangle(image, cv::Point(50, 50), cv::Point(300, 300), blue, 1, cv::LINE_4);
 
-        if(key == 27)
-        {
-            break;
-        }
-        else if(key == 'a')
-        {
-            std::cout<<"a = "<<key<<std::endl;
-        }
-    }
+    cv::putText(image, "hello world", cv::Point(100, 400), cv::FONT_HERSHEY_DUPLEX, 1, red, 1);
+    cv::putText(image, "hello world", cv::Point(100, 600), cv::FONT_HERSHEY_DUPLEX, 2, red, 4);
+    cv::circle(image, cv::Point(0, 0), 50, blue);
+    cv::ellipse(image, cv::Point(600,600),cv::Size(100,60),0,0,360,red,4);
 
-    cv::destroyAllWindows();
-
+    cv::imshow("test", image);
+    cv::waitKey(0);
 
     return 0;
 }
