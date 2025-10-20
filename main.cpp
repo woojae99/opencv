708a13300c22ec406e0283dd85c27988e2b4f0bb
WJ <boss403@naver.com>
2025-10-21 01:39:24 +0900

    이미지 read

--- main.cpp
+++ main.cpp
@@ -3,23 +3,18 @@
 
 int main(void)
 {
-    cv::Scalar blue(255, 0, 0), green(0, 255, 0), red(0, 0, 255), white(255, 255, 255);
-    cv::Mat image(800, 800, CV_8UC3, white);
+    std::string filename = "/home/wj/Pictures/icon.png";
 
-    cv::Point pt1(100, 100), pt2(200, 200), pt3(300, 300);
-    cv::Rect r1(pt1, cv::Size(50, 50));
+    cv::Mat image = cv::imread(filename,cv::IMREAD_COLOR);
 
-    cv::line(image, cv::Point(100, 100), cv::Point(200, 100), red, 1, cv::LINE_4);
-    cv::line(image, cv::Point(100, 110), cv::Point(200, 110), red, 1, cv::LINE_4);
+    cv::Vec3b pixel = image.at<cv::Vec3b>(100,200);
+    std::cout<<(int)pixel[0]<<std::endl;
+    std::cout<<(int)pixel[1]<<std::endl;
+    std::cout<<(int)pixel[2]<<std::endl;
 
-    cv::rectangle(image, cv::Point(50, 50), cv::Point(300, 300), blue, 1, cv::LINE_4);
+    image.at<cv::Vec3b>(5,5) = cv::Vec3b(0,0,255);
 
-    cv::putText(image, "hello world", cv::Point(100, 400), cv::FONT_HERSHEY_DUPLEX, 1, red, 1);
-    cv::putText(image, "hello world", cv::Point(100, 600), cv::FONT_HERSHEY_DUPLEX, 2, red, 4);
-    cv::circle(image, cv::Point(0, 0), 50, blue);
-    cv::ellipse(image, cv::Point(600,600),cv::Size(100,60),0,0,360,red,4);
-
-    cv::imshow("test", image);
+    cv::imshow("image", image);
     cv::waitKey(0);
 
     return 0;
