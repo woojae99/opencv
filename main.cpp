12892daf80e9516e8fa037fe6d8a75192f367008
WJ <boss403@naver.com>
2025-10-31 01:42:36 +0900

    웹캠 데이터 읽기

--- main.cpp
+++ main.cpp
@@ -3,33 +3,32 @@
 
 int main(void)
 {
-    std::string filename = "/home/wj/Videos/video.webm";
+    cv::VideoCapture cap(0);
 
-    cv::VideoCapture video(filename);
-
-    if (!video.isOpened())
+    if (!cap.isOpened())
     {
-        std::cerr << "동영상 파일을 열 수 없습니다: " << filename << std::endl;
-        std::cerr << "FFmpeg 또는 코덱이 설치되어 있는지 확인하세요." << std::endl;
+        std::cerr << "카메라 연결을 확인하세요. " << std::endl;
         return -1;
     }
 
-    std::cout << video.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
-    std::cout << video.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
-    std::cout << video.get(cv::CAP_PROP_ZOOM) << std::endl;
+    // std::cout << cap.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
+    // std::cout << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+    // std::cout << cap.get(cv::CAP_PROP_ZOOM) << std::endl;
 
-    video.set(cv::CAP_PROP_BRIGHTNESS, 0); // 이미 인코딩된 영상은 동작 x
+    cap.set(cv::CAP_PROP_BRIGHTNESS, 150);
+    cap.set(cv::CAP_PROP_AUTOFOCUS, 0);
+    cap.set(cv::CAP_PROP_ZOOM, 100);
 
     cv::Mat frame;
 
     while (1)
     {
-        if (!video.read(frame))
+        if (!cap.read(frame))
             break;
 
         cv::imshow("test", frame);
 
-        if (cv::waitKey(100) == 27)
+        if (cv::waitKey(10) == 27)
             break;
     }
 
