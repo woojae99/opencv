7b71d1e707fc13b6820681b22b52fc8186a6ce09
WJ <boss403@naver.com>
2025-10-31 01:27:50 +0900

    추가 영상 정보 출력

--- main.cpp
+++ main.cpp
@@ -7,24 +7,30 @@ int main(void)
 
     cv::VideoCapture video(filename);
 
-    if (!video.isOpened()) {
+    if (!video.isOpened())
+    {
         std::cerr << "동영상 파일을 열 수 없습니다: " << filename << std::endl;
         std::cerr << "FFmpeg 또는 코덱이 설치되어 있는지 확인하세요." << std::endl;
         return -1;
     }
 
-    std::cout<<video.get(cv::CAP_PROP_FRAME_WIDTH);
-    std::cout<<video.get(cv::CAP_PROP_FRAME_HEIGHT);
+    std::cout << video.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
+    std::cout << video.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+    std::cout << video.get(cv::CAP_PROP_ZOOM) << std::endl;
+
+    video.set(cv::CAP_PROP_BRIGHTNESS, 0); // 이미 인코딩된 영상은 동작 x
 
     cv::Mat frame;
 
-    while(1)
+    while (1)
     {
-        if(!video.read(frame)) break;
+        if (!video.read(frame))
+            break;
 
         cv::imshow("test", frame);
 
-        if(cv::waitKey(100) == 27) break;
+        if (cv::waitKey(100) == 27)
+            break;
     }
 
     return 0;
