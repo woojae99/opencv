980b209ded4a79cf8b836b59bf94fe216fd8a33f
WJ <boss403@naver.com>
2025-11-22 22:44:15 +0900

    image 회전 및 전치

--- main.cpp
+++ main.cpp
@@ -5,18 +5,20 @@ int main(void)
 {
     std::string filename = "/home/wj/Pictures/icon.png";
 
-    cv::Mat image = cv::imread(filename,cv::IMREAD_COLOR);
+    cv::Mat image1 = cv::imread(filename, cv::IMREAD_COLOR);
 
-    cv::Vec3b pixel = image.at<cv::Vec3b>(100,200);
-    std::cout<<(int)pixel[0]<<std::endl;
-    std::cout<<(int)pixel[1]<<std::endl;
-    std::cout<<(int)pixel[2]<<std::endl;
+    cv::Mat axis_x, axis_y, repeat, trans;
 
-    image.at<cv::Vec3b>(5,5) = cv::Vec3b(0,0,255);
-    image -= cv::Scalar(100, 0, 0);
-    image = image * 0.2;
+    cv::flip(image1, axis_x, 0);
+    cv::flip(image1, axis_y, 1);
+    cv::repeat(image1, 2, 1, repeat);
+    cv::transpose(image1,trans);
 
-    cv::imshow("image", image);
+    cv::imshow("show1", image1);
+    // cv::imshow("show2", axis_x);
+    // cv::imshow("show3", axis_y);
+    // cv::imshow("repeat",repeat);
+    cv::imshow("repeat",trans);
     cv::waitKey(0);
 
     return 0;
