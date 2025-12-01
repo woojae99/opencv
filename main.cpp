9ff59d263a71af107808c554a5ef74261bb56e22
WJ <boss403@naver.com>
2025-12-02 02:31:48 +0900

    최소 최대

--- main.cpp
+++ main.cpp
@@ -4,18 +4,25 @@
 
 int main(void)
 {
-    std::string filename1 = "/home/wj/Pictures/nature.jpeg";
-    std::string filename2 = "/home/wj/Pictures//icon.png";
+    unsigned char data[] = {10,200,5,7,9,15,35,68,35,100,2,55,37,90, 1};
 
-    cv::Mat image = cv::imread(filename1, cv::IMREAD_COLOR);
-    cv::Mat logo = cv::imread(filename2, cv::IMREAD_COLOR);
+    double minval, maxval;
+    int minidx[2], maxidx[2];
+    cv::Point minloc, maxloc;
 
-    cv::Mat result;
-    cv::resize(image,image,logo.size());
-    cv::subtract(logo,image,result);
+    cv::Mat m1(3,5,CV_8U,data);
+    cv::Mat m2 (3,5,CV_8U,cv::Scalar(50));
+    cv::Mat m_min, m_max;
 
-    cv::imshow("result", result);
-    cv::waitKey(0);
+    cv::min(m1,m2,m_min);
+    cv::max(m1,m2,m_max);
+
+    cv::minMaxIdx(m1, &minval,&maxval,minidx,maxidx);
+    cv::minMaxLoc(m1,0,0,&minloc,&maxloc);
+    
+    std::cout<<minval<<","<<maxval<<std::endl;
+    std::cout<<minidx[1]<<","<<minidx[0]<<std::endl;
+    std::cout<<minloc<<","<<maxloc<<std::endl;
 
     return 0;
 }
