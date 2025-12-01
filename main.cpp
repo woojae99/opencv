f5bf42e32dd309d09f77593408bf5866f91a56bf
WJ <boss403@naver.com>
2025-12-02 00:18:30 +0900

    이미지 뺄셈

--- main.cpp
+++ main.cpp
@@ -10,29 +10,12 @@ int main(void)
     cv::Mat image = cv::imread(filename1, cv::IMREAD_COLOR);
     cv::Mat logo = cv::imread(filename2, cv::IMREAD_COLOR);
 
-    cv::Mat logo_th, masks[5], background, foreground, dst;
-    cv::threshold(logo,logo_th, 70, 255, cv::THRESH_BINARY);
-    cv::split(logo_th,masks);
-    
-    cv::bitwise_or(masks[0],masks[1],masks[3]);
-    cv::bitwise_or(masks[2],masks[3],masks[3]);
-    cv::bitwise_not(masks[3],masks[4]);
+    cv::Mat result;
+    cv::resize(image,image,logo.size());
+    cv::subtract(logo,image,result);
 
-    cv::Point center1 = image.size()/2;
-    cv::Point center2 = logo.size()/2;
-    cv::Point start = center1 - center2;
-    cv::Rect roi(start,logo.size());
-
-    cv::bitwise_and(logo,logo,foreground,masks[3]);
-    cv::bitwise_and(image(roi),image(roi),background,masks[4]);
-
-    cv::add(foreground, background,dst);
-    dst.copyTo(image(roi));
-
-    cv::imshow("image", dst);
-    cv::imshow("image2", image);
+    cv::imshow("result", result);
     cv::waitKey(0);
-    
 
     return 0;
 }
