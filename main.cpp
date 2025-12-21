044aeaae6bd72d864071675f539a4da591c7fdf6
WJ <boss403@naver.com>
2025-12-22 01:56:19 +0900

    이미지 명암 조절

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 int main(void)
 {
     std::string filename1 = "/home/wj/Pictures/nature.jpeg";
-    std::string filename2 = "/home/wj/Pictures/icon.png";
     cv::Mat image1 = cv::imread(filename1, cv::IMREAD_COLOR);
-    cv::Mat image2 = cv::imread(filename2, cv::IMREAD_COLOR);
 
-    cv::Mat dst1, dst2;
-    cv::resize(image1, dst1, cv::Size(255, 255));
-    cv::resize(image2, dst2, cv::Size(255, 255));
+    cv::Scalar avg = mean(image1) / 2.0;
+    cv::Mat result1 = image1 * 2;
+    cv::Mat result2 = image1 * 2 - avg;
+    
 
-    cv::Mat result1 = dst1*0.5 + dst2*0.5;
-
-    cv::imshow("result",result1);
+    cv::imshow("raw", image1);
+    cv::imshow("result", result1);
+        cv::imshow("result2", result2);
+    
     cv::waitKey(0);
 
     return 0;
