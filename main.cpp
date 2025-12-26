c7888c2ca1524fa6459b8bd48aa7fb1a26060e8f
WJ <boss403@naver.com>
2025-12-26 13:18:18 +0900

    히스토그램 스트레칭

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 
 int main(void)
 {
-    std::string filename1 = "/home/wj/Pictures/nature.jpeg";
-    cv::Mat image1 = cv::imread(filename1, cv::IMREAD_COLOR);
+    std::string filename1 = "/home/wj/Pictures/dark_image.png";
+    cv::Mat image1 = cv::imread(filename1, 0);
+    image1 = image1*0.2;
+    cv::Mat result;
 
-    cv::Scalar avg = mean(image1) / 2.0;
-    cv::Mat result1 = image1 * 2;
-    cv::Mat result2 = image1 * 2 - avg;
-    
+    if(image1.empty())
+    {
+        return 1;
+    }
+
+    double minV = 0.0, maxV = 0.0;
+
+    cv::minMaxLoc(image1,&minV,&maxV);
+
+    if (maxV - minV < 1e-9) {
+        return 1;
+    }
+
+    image1.convertTo(result,CV_8UC1,255/(maxV-minV),-minV * 255.0 / (maxV - minV));
 
     cv::imshow("raw", image1);
-    cv::imshow("result", result1);
-        cv::imshow("result2", result2);
+    cv::imshow("result",result);
     
     cv::waitKey(0);
 
