57e010105d82a3077f9b4c53a680b7ab053951ca
WJ <boss403@naver.com>
2026-01-08 01:10:19 +0900

    이미지 블러링

--- main.cpp
+++ main.cpp
@@ -4,90 +4,43 @@
 
 int main(void)
 {
-    std::string filename1 = "/home/wj/Pictures/dark_image.png";
-    cv::Mat image1 = cv::imread(filename1, 0);
-    image1 = image1 * 0.2;
+    std::string filename1 = "/home/wj/Pictures/nature.jpeg";
+    cv::Mat image1 = cv::imread(filename1, 1);
 
     if (image1.empty())
     {
         return 1;
     }
 
+    float data[] = {1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9, 1.0f / 9};
+    cv::Mat mask(3, 3, CV_32F, data);
+    cv::Mat blur(image1.size(), CV_8UC3, cv::Scalar(0,0,0));
+    cv::Point m_h = mask.size() / 2;
+
+    for (int i = m_h.y; i < image1.rows; i++)
+    {
+        for (int j = m_h.x; j < image1.cols; j++)
+        {
+            cv::Vec3b sum;
+            for (int u = 0; u < mask.rows; u++)
+            {
+                for (int v = 0; v < mask.cols; v++)
+                {
+                    int y = i + u - m_h.y;
+                    int x = j + v - m_h.x;
+                    float w = mask.at<float>(u,v);
+                    cv::Vec3b tmp = image1.at<cv::Vec3b>(y, x);
+                    sum[0] += w * (float)tmp[0];
+                    sum[1] += w * (float)tmp[1];
+                    sum[2] += w * (float)tmp[2];
+                }
+            }
+            blur.at<cv::Vec3b>(i, j) = cv::Vec3b((uchar)sum[0],(uchar)sum[1],(uchar)sum[2]);
+        }
+    }
+
     cv::imshow("raw", image1);
-
-    int L = 256;
-    int total = image1.rows * image1.cols;
-
-    // histogram
-    std::vector<int> hist(L, 0);
-    for (int y = 0; y < image1.rows; y++)
-    {
-        for (int x = 0; x < image1.cols; x++)
-        {
-            uchar tmp = image1.at<uchar>(y, x);
-            hist[tmp]++;
-        }
-    }
-
-    // cdf 누적합
-    std::vector<int> cdf(L, 0);
-    int tmp = 0;
-
-    for (int i = 0; i < L; i++)
-    {
-        tmp += hist[i];
-        cdf[i] = tmp;
-    }
-
-    // cdf 최소값
-    int cdf_min = 0;
-
-    for (int i = 0; i < L; i++)
-    {
-        if (cdf[i] != 0)
-        {
-            cdf_min = cdf[i];
-            break;
-        }
-    }
-
-    //lut 만들기
-    std::vector<uchar> lut(L, 0);
-
-    if (total == cdf_min)
-    {
-        for (int i = 0; i < L; ++i)
-        {
-            lut[i] = static_cast<uchar>(i);
-        }
-    }
-    else
-    {
-        for (int i = 0; i < L; i++)
-        {
-            float norm = float(cdf[i]-cdf_min)/float(total - cdf_min);
-            int mapped = int(norm*255.0 + 0.5f);
-            if (mapped < 0) mapped = 0;
-            if (mapped > 255) mapped = 255;
-
-            lut[i] = uchar(mapped);
-        }
-    }
-
-    // result
-    cv::Mat result(image1.size(), image1.type());
-
-    for(int y=0;y<image1.rows;y++)
-    {
-        for(int x=0;x<image1.cols;x++)
-        {
-            uchar v = image1.at<uchar>(y, x);   // 입력 픽셀 밝기(0~255)
-            result.at<uchar>(y, x) = lut[v];
-        }
-    }
-
-    cv::imshow("result",result);
-
+    cv::imshow("blur", blur);
     cv::waitKey(0);
 
     return 0;
