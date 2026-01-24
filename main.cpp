a08c2d9213966c65962b913c25ca3546505bec19
WJ <boss403@naver.com>
2026-01-25 02:22:08 +0900

    라플라시안 마스크

--- main.cpp
+++ main.cpp
@@ -2,35 +2,32 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-float data1[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-float data2[] = {1, 1, 1, 0, 0, 0, -1, -1, -1};
-// float data2[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+float data1[] = {0, 1, 0, 1, -4, 1, 0, 1, 0};
+float data2[] = {1, 1, 1, 1, -8, 1, 1, 1, 1};
 
 int main(void)
 {
     std::string filename1 = "/home/wj/Pictures/nature.jpeg";
-    cv::Mat image = cv::imread(filename1, 0);
-    cv::Mat dst, dst1, dst2, mask1(3, 3, CV_32F, data1), mask2(3, 3, CV_32F, data2);
+    cv::Mat image = cv::imread(filename1,0);
+    cv::Mat dst,dst1,dst2,mask1(3,3,CV_32F,data1), mask2(3,3,CV_32F,data2);
 
-    if (image.empty())
+    if(image.empty())
     {
         return 1;
     }
 
+
     cv::filter2D(image,dst1,CV_32F,mask1);
     cv::filter2D(image,dst2,CV_32F,mask2);
-    cv::magnitude(dst1,dst2,dst);
-    dst.convertTo(dst,CV_8U);
-    dst1.convertTo(dst1,CV_8U);
-    dst2.convertTo(dst2,CV_8U);
+    cv::Laplacian(image,dst,CV_32F,1);
+    cv::convertScaleAbs(dst1,dst1);
+    cv::convertScaleAbs(dst2,dst2);
+    cv::convertScaleAbs(dst,dst);
 
-    cv::imshow("raw",image);
-    cv::imshow("result",dst);
-    cv::imshow("result_x",dst1);
-    cv::imshow("result_y",dst2);
+    cv::imshow("1",dst1);
+    cv::imshow("2",dst2);
+    cv::imshow("laplacian",dst);
     cv::waitKey(0);
-    
-    
 
     return 0;
 }
