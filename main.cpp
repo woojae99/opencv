49f4d0c440626bb38f716768c624a40254790595
WJ <boss403@naver.com>
2026-01-27 23:58:10 +0900

    가우시안 블러링

--- main.cpp
+++ main.cpp
@@ -8,26 +8,24 @@ float data2[] = {1, 1, 1, 1, -8, 1, 1, 1, 1};
 int main(void)
 {
     std::string filename1 = "/home/wj/Pictures/nature.jpeg";
-    cv::Mat image = cv::imread(filename1,0);
-    cv::Mat dst,dst1,dst2,mask1(3,3,CV_32F,data1), mask2(3,3,CV_32F,data2);
+    cv::Mat image = cv::imread(filename1, 0);
+    cv::Mat result;
 
-    if(image.empty())
+    if (image.empty())
     {
         return 1;
     }
 
+    cv::Size size(5, 5);
+    double sigmaX = 0.3 * ((size.width * 0.5) - 1) + 0.8;
+    double sigmaY = 0.3 * ((size.height * 0.5) - 1) + 0.8;
 
-    cv::filter2D(image,dst1,CV_32F,mask1);
-    cv::filter2D(image,dst2,CV_32F,mask2);
-    cv::Laplacian(image,dst,CV_32F,1);
-    cv::convertScaleAbs(dst1,dst1);
-    cv::convertScaleAbs(dst2,dst2);
-    cv::convertScaleAbs(dst,dst);
+    cv::GaussianBlur(image,result,size,sigmaX,sigmaY);
 
-    cv::imshow("1",dst1);
-    cv::imshow("2",dst2);
-    cv::imshow("laplacian",dst);
+    cv::imshow("result",result);
+    cv::imshow("raw",image);
     cv::waitKey(0);
+    
 
     return 0;
 }
