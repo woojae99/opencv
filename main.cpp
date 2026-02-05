f66212ba8e4cb614b4f7f8e3887c44b53f211fd6
WJ <boss403@naver.com>
2026-02-05 23:14:10 +0900

    미디언 필터

--- main.cpp
+++ main.cpp
@@ -2,35 +2,6 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-void minMaxFilter(cv::Mat image, cv::Mat &dst, int size, int flag = 1)
-{
-    dst = cv::Mat(image.size(), CV_8U, cv::Scalar(0));
-    cv::Size msize(size, size);
-    cv::Point mid = msize / 2;
-
-    for (int i = mid.y; i < image.rows - mid.y; i++)
-    {
-        for (int k = mid.x; k < image.cols - mid.x; k++)
-        {
-            cv::Point start = cv::Point(k, i) - mid;
-            cv::Rect roi(start, msize);
-            cv::Mat mask = image(roi);
-
-            double min, max;
-            cv::minMaxLoc(mask, &min, &max);
-
-            if (flag == 1)
-            {
-                dst.at<uchar>(i, k) = max;
-            }
-            else
-            {
-                dst.at<uchar>(i, k) = min;
-            }
-        }
-    }
-}
-
 int main(void)
 {
     std::string filename1 = "/home/wj/Pictures/nature.jpeg";
@@ -42,12 +13,29 @@ int main(void)
         return 1;
     }
 
-    minMaxFilter(raw_image, min_img, 5, 0);
-    minMaxFilter(raw_image, max_img, 5, 1);
+    cv::Mat dst(raw_image.size(), CV_8U, cv::Scalar(0));
+    cv::Size msize(5, 5);
+    cv::Point mid = msize / 2;
 
-    cv::imshow("raw", raw_image);
-    cv::imshow("min", min_img);
-    cv::imshow("max", max_img);
+    for(int i=mid.y;i<raw_image.rows-mid.y;i++)
+    {
+        for(int k=mid.x;k<raw_image.cols-mid.x;k++)
+        {
+            cv::Point start = cv::Point(k,i) - mid;
+            cv::Rect roi(start,msize);
+            cv::Mat mask, sort;
+            raw_image(roi).copyTo(mask);
+
+            cv::Mat one_row = mask.reshape(1,1);
+            cv::sort(one_row,sort,cv::SORT_EVERY_ROW);
+
+            int median_idx = (int)one_row.total()/2;
+            dst.at<uchar>(i,k) = sort.at<uchar>(median_idx);
+        }
+    }
+
+    cv::imshow("raw_image", raw_image);
+    cv::imshow("result", dst);
     cv::waitKey(0);
 
     return 0;
