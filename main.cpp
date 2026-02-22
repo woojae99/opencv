9ccef9fd497cb919c83aee3ae2a6012f6d70b869
WJ <boss403@naver.com>
2026-02-22 17:15:11 +0900

    모폴로지 열림 닫힘

--- main.cpp
+++ main.cpp
@@ -2,63 +2,32 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-bool check_match(cv::Mat img, cv::Point start, cv::Mat mask, int mode = 0)
-{
-    for (int u = 0 ; u< mask.rows;u++)
-    {
-        for(int v = 0;v<mask.cols;v++)
-        {
-            cv::Point pt(v,u);
-            int m = mask.at<uchar>(pt);
-            int p = img.at<uchar>(start+pt);
-
-            bool ch = (p==255);
-            if(m==1 && ch==mode)
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-void dliation(cv::Mat img, cv::Mat& dst, cv::Mat mask)
-{
-    dst = cv::Mat(img.size(), CV_8U, cv::Scalar(0));
-
-    cv::Point mid = mask.size() / 2;
-    for (int i = mid.y; i < img.rows - mid.y; i++)
-    {
-        for(int k = mid.x; k<img.cols - mid.x;k++)
-        {
-            cv::Point start = cv::Point(k,i) - mid;
-            bool check = check_match(img,start,mask,1);
-            dst.at<uchar>(i,k) = (check) ? 0:255;
-        }
-    }
-}
-
 int main(void)
 {
     std::string filename1 = "/home/wj/Pictures/nature.jpeg";
     cv::Mat raw_image = cv::imread(filename1, 0);
-    cv::Mat th_img, dst1;
+    cv::Mat th_img, open, close;
 
     if (raw_image.empty())
     {
         return 1;
     }
 
-    cv::threshold(raw_image,th_img,128,255,cv::THRESH_BINARY);
+    cv::threshold(raw_image, th_img, 128, 255, cv::THRESH_BINARY);
 
-    uchar data[] = {0,1,0,1,1,1,0,1,1};
-    cv::Mat mask(3,3,CV_8UC1,data);
+    cv::Mat mask = (cv::Mat_<uchar>(3,3) <<
+    0,1,0,
+    1,1,1,
+    0,1,0
+);
 
-    dliation(th_img,dst1,(cv::Mat)mask);
+    cv::morphologyEx(th_img, open, cv::MORPH_OPEN, mask, cv::Point(-1, -1), 1);
+    cv::morphologyEx(th_img, close, cv::MORPH_CLOSE, mask, cv::Point(-1, -1), 1);
 
     cv::imshow("raw", raw_image);
-    cv::imshow("dst", dst1);
-    cv::waitKey(0);    
+    cv::imshow("open", open);
+    cv::imshow("close", close);
+    cv::waitKey(0);
 
     return 0;
 }
