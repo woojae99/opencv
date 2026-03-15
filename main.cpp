3d2f937f78f3c7dd48d3c3bbc88fc95da4fd002b
WJ <boss403@naver.com>
2026-03-16 00:22:39 +0900

    이미지 선형보간

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-void scaling_near(cv::Mat img, cv::Mat &dst, cv::Size size)
+uchar bilinear_value(cv::Mat img, double x, double y)
+{
+    cv::Point pt((int)x, (int)y);
+    int a = img.at<uchar>(pt);
+    int b = img.at<uchar>(pt + cv::Point(0, 1));
+    int c = img.at<uchar>(pt + cv::Point(1, 0));
+    int d = img.at<uchar>(pt + cv::Point(1, 1));
+
+    double alpha = y - pt.y;
+    double beta = x - pt.x;
+    int m1 = a + (int)cvRound(alpha*(b-a));
+    int m2 = c + (int)cvRound(alpha*(d-c));
+    int p = m1 + (int)cvRound(m2-m1);
+    return cv::saturate_cast<uchar>(p);
+}
+
+void bilinear(cv::Mat img, cv::Mat &dst, cv::Size size)
 {
     dst = cv::Mat(size, img.type(), cv::Scalar(0));
     double ratioy = (double)size.height / img.rows;
@@ -12,10 +28,10 @@ void scaling_near(cv::Mat img, cv::Mat &dst, cv::Size size)
     {
         for (int k = 0; k < dst.cols; k++)
         {
-            int x = (int)cvRound(k/ratiox);
-            int y = (int)cvRound(i/ratioy);
+            int x = (int)cvRound(k / ratiox);
+            int y = (int)cvRound(i / ratioy);
 
-            dst.at<char>(i,k) = img.at<char>(y,x);
+            dst.at<char>(i, k) = bilinear_value(img, x, y);
         }
     }
 }
@@ -30,12 +46,15 @@ int main(void)
         return 1;
     }
 
-    cv::Mat dst;
-    scaling_near(raw_image,dst,cv::Size(1000,700));
+    cv::Mat dst,dst2;
+    bilinear(raw_image, dst, cv::Size(1000, 700));
 
-    cv::imshow("image",raw_image);
-    cv::imshow("dst",dst);
-    cv::waitKey(0);    
+    cv::resize(raw_image,dst2,cv::Size(1000,700),0,0,cv::INTER_NEAREST);
+
+    cv::imshow("image", raw_image);
+    cv::imshow("dst", dst);
+    cv::imshow("dst2",dst2);
+    cv::waitKey(0);
 
     return 0;
 }
